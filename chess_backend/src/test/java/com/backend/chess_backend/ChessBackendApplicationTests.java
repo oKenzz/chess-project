8399a4fd96914f0e6b11c11f247eead6b7f0dfdf
oKenzz <ken98ph@gmail.com>
2023-11-22 18:00:55 +0100

    Removed: Duplicate test

--- chess_backend/src/test/java/com/backend/chess_backend/ChessBackendApplicationTests.java
+++ chess_backend/src/test/java/com/backend/chess_backend/ChessBackendApplicationTests.java
@@ -17,52 +17,8 @@ import com.backend.chess_backend.model.Translator;
 @SpringBootTest
 class ChessBackendApplicationTests {
 
-
     @Test
-    public void translateBoardTest()
-    {
-        Translator translator = new Translator();
-        Piece[][] board = new Piece[8][8];
+	void contextLoads() {
+	}
 
-        // for(int x = 0; x < board.length; x++){
-        //     for(int y = 0; y < board[0].length; y++){
-
-        //         if(x % 2 == 0){
-        //             board[x][y] = new Rook(PieceColor.WHITE, x, y);
-        //         }else{
-        //             board[x][y] = null;
-        //         }
-        //     }
-        // }
-        // String fen = translator.translateBoard(board);
-        // assertEquals("R1R1R1R1/R1R1R1R1/R1R1R1R1/R1R1R1R1/R1R1R1R1/R1R1R1R1/R1R1R1R1/R1R1R1R1", fen);
-        
-        PieceColor currentColor = PieceColor.WHITE;
-        for (int y = 0; y < board[0].length; y++){
-            if(y < 4){
-                currentColor = PieceColor.WHITE;
-            }else{
-                currentColor = PieceColor.BLACK;
-            }
-            for(int x = 0; x < board.length; x++){
-                if(y == 1 || y == 6){
-                    board[x][y] = new Pawn(currentColor, x, y);
-                }else if(y == 0 || y == 7 ){
-                    if (x==0 || x == 7){
-                        board[x][y] = new Rook(currentColor, x, y);
-                    }else if (x==1 || x == 6) {
-                        board[x][y] = new Knight(currentColor, x, y);
-                    }else if (x==2 || x == 5) {
-                        board[x][y] = new Bishop(currentColor, x, y);
-                    }else if (x==3) {
-                        board[x][y] = new Queen(currentColor, x, y);
-                    }else if (x==4) {
-                        board[x][y] = new King(currentColor, x, y);
-                    }
-                }    
-            }
-        }
-        String fen = translator.translateBoard(board);
-        assertEquals("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR", fen);
-    }
 }
