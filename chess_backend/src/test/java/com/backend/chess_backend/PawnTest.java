afb020c47285d449aa94bac38933bf6fbcdc7fc7
IsmailR16 <ismaelalhoran585@gmail.com>
2023-11-22 22:25:19 +0100

    Tested Pawn, King and Knight

---
+++ chess_backend/src/test/java/com/backend/chess_backend/PawnTest.java
@@ -0,0 +1,32 @@
+package com.backend.chess_backend;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.backend.chess_backend.model.Game;
+import com.backend.chess_backend.model.Pieces.Pawn;
+import com.backend.chess_backend.model.Pieces.Piece;
+import com.backend.chess_backend.model.Pieces.PieceColor;
+
+@SpringBootTest
+class PawnTest {
+
+    @Test
+    public void TestGetPossibleMoves(){
+        Piece pawn = new Pawn(PieceColor.WHITE, 0, 1);
+        Boolean[][] BoardList = new Boolean[8][8];
+
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                BoardList[i][j] = false;
+            }
+        } 
+        BoardList[0][2] = true;
+        BoardList[0][3] = true;
+
+        assertArrayEquals(pawn.getPossibleMoves(8, 8), BoardList);
+    }
+
+}
