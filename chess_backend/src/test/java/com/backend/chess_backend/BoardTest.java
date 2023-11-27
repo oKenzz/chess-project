287460d4ac47c57632902cd5ead47db9c7a3fa7e
pontus <pontusgi@chalmer.se>
2023-11-27 14:27:33 +0100

    board tests

--- chess_backend/src/test/java/com/backend/chess_backend/BoardTest.java
+++ chess_backend/src/test/java/com/backend/chess_backend/BoardTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import com.backend.chess_backend.model.Board;
 import com.backend.chess_backend.model.Game;
+import com.backend.chess_backend.model.Translator;
 import com.backend.chess_backend.model.Pieces.Piece;
 
 @SpringBootTest
@@ -26,5 +27,35 @@ public class BoardTest {
         Boolean[][] testlist=board.getPossibleMoves(tmp[4][4]);
         assertTrue(board.checkIfallowed(5, 5, testlist));
     }
+    @Test
+    public void createBoardTest(){
+        Board board=new Board();
+        Piece[][] tmp=board.getBoard();
+        board.reset();
+        Translator translator = new Translator();
+        String fen = translator.translateBoard(tmp);
+        assertEquals("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR", fen);
+    }
+    @Test
+    public void possiblemovetest(){
+        Board board=new Board();
+        board.reset();
+        Piece[][] tmp=board.getBoard();
+        //for (int i=0; i<7; i++){
+        Boolean[][] moves=board.getPossibleMoves(tmp[5][6]);
+        assertTrue(moves[5][5]);
+        assertTrue(moves[5][4]);
+        assertTrue(!moves[4][5]);
+        assertTrue(!moves[6][5]);
+        // for (int x=0; x<8;x++){
+        //     Boolean[][] moves=board.getPossibleMoves(tmp[x][7]);
+        //     assertEquals("p", tmp[x][6].getPieceType());
+
+            // for(int y=0;y<8;y++){
+            //    assertTrue(!moves[x][y]);
+            // }
+        //}
+    //}
+}
     
 }
