397f65e7c6a9226ce6409025fe51d674bfdd51f3
Melker Gustafsson <smelker02@gmail.com>
2023-11-29 13:16:59 +0100

    made new branch
    Co-authored-by: IsmailR16 <IsmailR16@users.noreply.github.com>

--- chess_backend/src/main/java/com/backend/chess_backend/socket/ChessHandler.java
+++ chess_backend/src/main/java/com/backend/chess_backend/socket/ChessHandler.java
@@ -1,24 +1,35 @@
 package com.backend.chess_backend.socket;
 
-import com.backend.chess_backend.model.Game;
-import com.corundumstudio.socketio.AckRequest;
-import com.corundumstudio.socketio.SocketIOClient;
-import com.corundumstudio.socketio.listener.ConnectListener;
-import com.corundumstudio.socketio.listener.DisconnectListener;
-import lombok.extern.log4j.Log4j2;
+import java.util.ArrayList;
 
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import com.backend.chess_backend.model.Game;
+import com.backend.chess_backend.model.Translator;
+import com.corundumstudio.socketio.AckRequest;
+import com.corundumstudio.socketio.SocketIOClient;
+import com.corundumstudio.socketio.SocketIOServer;
+import com.corundumstudio.socketio.listener.ConnectListener;
+import com.corundumstudio.socketio.listener.DisconnectListener;
+
+import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 @Component
 public class ChessHandler {
 
     private final GameManager gameManager;
+    private final Translator translator;
+    private final SocketIOServer server; // SocketIOServer instance
 
     @Autowired
-    public ChessHandler(GameManager gameManager) {
+    public ChessHandler(GameManager gameManager, Translator translator, SocketIOServer server) {
         this.gameManager = gameManager;
+        this.translator = translator;
+        this.server = server;
+
     }
 
     // Listener for client connection events
@@ -38,12 +49,13 @@ public class ChessHandler {
                 log.info("No room was found. Attempting to join a random room.");
                 Game joinedGame = gameManager.joinRandomGame(sessionId);
                 if (joinedGame != null) {
-                String roomCode = joinedGame.getId();
-                client.joinRoom(roomCode);
-                log.info("Socket ID[{}] - room[{}] - Connected to chess game", sessionId, roomCode);
+                    String roomCode = joinedGame.getId();
+                    client.joinRoom(roomCode);
+                    log.info("Socket ID[{}] - room[{}] - Connected to chess game", sessionId, roomCode);
                 } else {
-                log.info("Player is already in a game. Not joining a new game.");
-                // Optionally, send a message to the client about not being able to join a new game.
+                    log.info("Player is already in a game. Not joining a new game.");
+                    // Optionally, send a message to the client about not being able to join a new
+                    // game.
                 }
             }
         };
@@ -52,7 +64,36 @@ public class ChessHandler {
     public void onChatMessage(SocketIOClient client, String message, AckRequest ackRequest) {
         log.info("Message: " + message + " From: " + client.getSessionId());
         // send message to global chat
-        client.getNamespace().getBroadcastOperations().sendEvent("chat", message);
+        server.getBroadcastOperations().sendEvent("chat", message);
+
+        if (ackRequest.isAckRequested()) {
+            ackRequest.sendAckData("Message has been receieved");
+        }
+    }
+
+    // For starting a new game and reconnecting to a game
+    public void getState(SocketIOClient client, AckRequest ackRequest) {
+        // Return board state (FEN), player turn, and player color, time remaining, etc.
+        JSONObject gameData = new JSONObject();
+
+        String playerUuID = client.getSessionId().toString();
+        Game game = gameManager.getGameByPlayerUuid(playerUuID);
+
+        // {
+        // "fen": "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR",
+        // "turn": "w",
+        // "playerColor": "w",
+        // "gameStatus": "inProgress",
+        // "gameCreatedAt": "2021-03-21T20:20:20.000Z",
+        // "players":{
+        // "w": {
+        // "timeRemaining": 300,
+        // },
+        // "b": {
+        // "timeRemaining": 300,
+        // }
+        // }
+        // }
 
         if (ackRequest.isAckRequested()) {
             ackRequest.sendAckData("Message has been receieved");
@@ -60,14 +101,27 @@ public class ChessHandler {
     }
 
     public void moveListener(SocketIOClient client, String move, AckRequest ackRequest) {
-        log.info("Move: " + move + " From: " + client.getSessionId());
+        // Model do move and return board state
+        String playerUuID = client.getSessionId().toString();
+        JSONObject jsonObject = new JSONObject(move);
+        String sourceSquare = jsonObject.getString("from");
+        String targetSquare = jsonObject.getString("to");
+        log.info("Move: " + sourceSquare + " to " + targetSquare + " From: " + client.getSessionId());
 
-        String fen = "8/5k2/3p4/1p1Pp2p/pP2Pp1P/P4P1K/8/8 b - - 99 50";
-        client.getNamespace().getBroadcastOperations().sendEvent("gameState", fen);
-        if (ackRequest.isAckRequested()) {
-            ackRequest.sendAckData(true);
+        ArrayList<ArrayList<Integer>> coordinates = translator.translatePos(sourceSquare, targetSquare);
+        ArrayList<Integer> oldCord = coordinates.get(0);
+        ArrayList<Integer> newCord = coordinates.get(1);
+
+        log.info("Translated to " + coordinates);
+        Game game = gameManager.getGameByPlayerUuid(playerUuID);
+
+        Boolean hasMoved = game.attemptMove(oldCord.get(0), oldCord.get(1), newCord.get(0), newCord.get(1));
+
+        if (hasMoved) {
+            String fen = translator.translateBoard(game.getBoard());
+            server.getRoomOperations(game.getId()).sendEvent("gameState", fen);
         }
-        
+
     }
 
     public void newGamePostionListener(SocketIOClient client, String fen, AckRequest ackRequest) {
@@ -76,7 +130,7 @@ public class ChessHandler {
         // GameManager gameManager = GameManager.getInstance();
         String roomID = gameManager.getGameIdByPlayerUuid(client.getSessionId().toString());
         log.info("Sending new game position to room: " + roomID);
-        client.getNamespace().getRoomOperations(roomID).sendEvent("gameState", fen);
+        server.getRoomOperations(roomID).sendEvent("gameState", fen);
 
         if (ackRequest.isAckRequested()) {
             ackRequest.sendAckData(true);
