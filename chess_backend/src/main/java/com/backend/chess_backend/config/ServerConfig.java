f172eda08c083b3377a4d38fc4a0f86badf42391
Ismail Sacic <cicas03@hotmail.com>
2023-12-06 21:08:34 +0100

    fixed origin thing

--- chess_backend/src/main/java/com/backend/chess_backend/config/ServerConfig.java
+++ chess_backend/src/main/java/com/backend/chess_backend/config/ServerConfig.java
@@ -11,6 +11,7 @@ public class ServerConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
